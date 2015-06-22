de3c9432d90cb6ebf3a53ce830a5c7343a24ed9c
istobarton <istobarton@gmail.com>
2015-06-22 12:35:36 -0500

    Began basic app methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,54 @@
 // YOUR CODE HERE:
 
+var app = {
+  init : function(){},
+
+  send : function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  fetch : function(url){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: url,
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message fetched');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message');
+      }
+    });
+  },
+
+  addMessage : function(message){
+
+
+    $('#chatbox').prepend("<div class='message'>"+message+"</div>")
+  },
+
+  clearMessages : function(){
+
+    $(".message").remove();
+  }
+
+
+};
+
+
