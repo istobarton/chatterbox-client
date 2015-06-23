3dda41f662f92f53317697dfd061935768463d68
istobarton <istobarton@gmail.com>
2015-06-23 12:34:23 -0500

    Friend, Room basic functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,7 +4,12 @@ var uzerString = window.location.search;
 var uzerArr = uzerString.split("=");
 var uzer = uzerArr[1];
 
+
+
 var app = {
+
+  //friend storage
+  friendStorage: {},
 
   init : function(){
 
@@ -25,14 +30,19 @@ var app = {
       $(".new").on("click", function(){
         app.fetch('https://api.parse.com/1/classes/chatterbox');
         app.clearMessages();
+        app.clearRooms();
       });
 
       $(".clean").on("click", function(){
         app.clearMessages();
+        app.clearRooms();
+        app.clearFriends();
       });
 
     })
 
+
+      // app.addFriend(this);
 
 
   },
@@ -69,17 +79,70 @@ var app = {
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
-        // console.log(data)
+        console.log(data)
 
+        var storage = {};
         var arr = data['results'];
+
         //loop through results array
         for (var i = 0; i < 50; i ++){
+
           //stringify key
           var key = i + '';
+
+          //check if a friend exists
+          if ((arr[key]["username"]) in app.friendStorage) {
+            //change color to blue
+            console.log(arr[key]["username"]);
+            $()
+          }
           //add messages
-          app.addMessage(arr[i]);
+          app.addMessage(arr[key]);
+
+          //-----------------------
+
+          // console.log(arr[key]["roomname"]);
+          if (arr[key]["roomname"] === undefined || arr[key]["roomname"] === '') {
+            i++;
+          } else {
+
+            storage[arr[key]["roomname"]] = arr[key]["roomname"]
+
+          }
+
+          // -----------------------
+
 
         };
+
+        //calls addRoom for each unique room
+        for (var item in storage){
+          app.addRoom(storage[item]);
+        }
+
+
+        //friend listener
+        $(".user").on("click", function(){
+          //capture current friend
+          var currentFriend = $(this).text();
+
+          if(!app.friendStorage[currentFriend]){
+            app.addFriend($(this).text());
+            app.friendStorage[currentFriend] = currentFriend;
+
+            //change friend color
+            for (var j = 0; j < $('.user').length; j++){
+              var $current = $(this)
+              var $compare = $($(".user")[j])
+
+              if ($current.text() === $compare.text()) {
+                $current.css({'color':'blue'});
+                $compare.css({'color':'blue'});
+              }
+            }
+          }
+        });
+
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -90,13 +153,24 @@ var app = {
 
   addMessage : function(obj){
 
-
     var $message = $("<div class='message'></div>");
-    $message.text(obj["username"] + ": " + obj['text'])
+
+    var $username = $("<span class='user'></span>");
+    $username.text(obj["username"]);
+
+    var $text = $("<span></span>")
+    $text.text(": " + obj["text"]);
+
+
+    $message.append($username)
+    $message.append($text)
+
+    // $message.append($username + ": " + $text);
+
+    // $message.text(": " + obj['text']);
+
 
     $("#chats").append($message);
-
-
   },
 
   clearMessages : function(){
@@ -104,12 +178,41 @@ var app = {
   },
 
   addRoom : function(roomName){
-    $("#roomSelect").append("<div class='room'>" + roomName + "</div>");
+    //sanitize
+    var $roomList = $("<li></li>");
+    $roomList.text(roomName);
+    $(".rooms").append($roomList);
+  },
+
+  clearRooms: function(){
+    $('.rooms').empty();
   },
 
   addFriend : function(username){
-    console.log("test");
-    // $('#chats').append("<a class='friend'>" + username + "</a>")
+    // console.log(this);
+
+    var $friendList = $("<li></li>")
+    $friendList.text(username);
+    $(".friends").append($friendList);
+    // $(".friends").append("<li>" + username + "</li>");
+  },
+
+  // colorFriend: function(name){
+
+  //   for (var item in app.friendStorage){
+  //     if (name === item){
+  //       for(var i = 0; i < $(".user").length); i++){
+  //         if()
+  //       }
+  //     }
+  //   }
+  // }
+
+  //             app.addFriend($(this).text());
+
+
+  clearFriends : function(){
+    $('.friends').empty();
   }
 
 
